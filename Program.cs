e091b442416f81adea621e953197fc7c29ac18ca
Vahdet Savci <savcivahdet@gmail.com>
2023-11-01 16:48:25 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace generic_list;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> sayiListesi = new List<int>();
+        sayiListesi.Add(23);
+        sayiListesi.Add(10);
+        sayiListesi.Add(4);
+        sayiListesi.Add(5);
+        sayiListesi.Add(92);
+        sayiListesi.Add(34);
+
+        List<string> renkListesi = new List<string>();
+        renkListesi.Add("Kırmızı");
+        renkListesi.Add("Mavi");
+        renkListesi.Add("Turuncu");
+        renkListesi.Add("Sarı");
+        renkListesi.Add("Yeşil");
+
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# PatikaAcademy.dev / CSharp-101 / Generic Koleksiyonlar ve List
+Bu çalışma **Generic Koleksiyonlar ve List** dersi için yapılmıştır. İçerisinde 1 ***.gitignore***, 1 ***README***, 1 ***LICENSE*** dosyası ve 1 ***Console*** uygulama barındırır.
+
+## LICENSE
+[MIT](LICENSE)
